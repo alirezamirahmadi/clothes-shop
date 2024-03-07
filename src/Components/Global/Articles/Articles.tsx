48b95771abaf8e46ec5979b22d60fd6e235af395
Alireza Mirahmadi <alireza.mirahmadi@gmail.com>
2024-03-07 23:18:20 +0330

    refactor:article pagination with react-qury

--- src/Components/Global/Articles/Articles.tsx
+++ src/Components/Global/Articles/Articles.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom'
+// import { useParams } from 'react-router-dom'
 import { Alert, Grid, useTheme, Box } from '@mui/material'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination as SwiperPagination } from 'swiper/modules';
@@ -16,23 +16,25 @@ import BorderOne from "../Border/BorderOne";
 import { ArticleType } from "../../../Utils/Types";
 import { ArticleProp } from "../../../Utils/Types";
 import { PaginationType } from "../../../Utils/Types";
-import { useArticle } from "../../../Hooks/ArticleHook";
+import { useArticlePagination, useArticleFilter } from "../../../Hooks/ArticleHook";
 
 export default function Articles({ filter, showPagination }: ArticleProp) {
 	// const dispatch: AppDispatch = useDispatch();
 	// const articles = useSelector((state: RootState) => state.articles);
-	const { data } = useArticle();
-	const [currentArticles, setCurrentArticles] = useState<ArticleType[]>(data);
-	const [filterArticles, setFilterArticles] = useState<ArticleType[]>(data);
+	// const [currentArticles, setCurrentArticles] = useState<ArticleType[]>(data);
+	// const [filterArticles, setFilterArticles] = useState<ArticleType[]>(data);
+	const [articles, setArticles] = useState<ArticleType[]>([]);
 	const [pagination, setPagination] = useState<PaginationType>();
 	const [currentPage, setCurrentPage] = useState(1);
-	const [pageSize,] = useState(6);
-	const ArticleParams = useParams();
+	const [perPage,] = useState(6);
+	const { data } = useArticlePagination(currentPage, perPage);
+	const latestArticles = useArticleFilter('latest').data;
+	// const ArticleParams = useParams();
 	const theme = useTheme();
 
 	const createPagination = () => {
 		setPagination({
-			pageCount: Math.ceil(filterArticles?.length && pageSize ? filterArticles?.length / pageSize : 1),
+			pageCount: data?.pages, // Math.ceil(filterArticles?.length && perPage ? filterArticles?.length / perPage : 1),
 			currentPage,
 			pageNoHandler,
 			justifyContent: 'center',
@@ -41,8 +43,8 @@ export default function Articles({ filter, showPagination }: ArticleProp) {
 			first: false,
 			last: false,
 		})
-		let tempArray: ArticleType[] = filterArticles ? [...filterArticles] : []
-		setCurrentArticles([...tempArray.splice((currentPage - 1) * pageSize, pageSize)])
+		// let tempArray: ArticleType[] = filterArticles ? [...filterArticles] : []
+		// setCurrentArticles([...tempArray.splice((currentPage - 1) * perPage, perPage)])
 	}
 
 	const pageNoHandler = (pageNo: number) => {
@@ -52,32 +54,16 @@ export default function Articles({ filter, showPagination }: ArticleProp) {
 	// useEffect(() => {
 	// 	dispatch(getArticlesFromServer());
 	// }, [])
-	useEffect(() => {
-		(filter === 'all' || !filter) && createPagination()
-	}, [filterArticles])
+	// useEffect(() => {
+	// 	(filter === 'all' || !filter) && createPagination()
+	// }, [filterArticles])
 
 	useEffect(() => {
-		setCurrentArticles(data)
-		setFilterArticles(data)
+		setArticles(data?.data);
+		createPagination();
+		// setCurrentArticles(data)
+		// setFilterArticles(data)
 	}, [data])
-
-	useEffect(() => {
-		if (filter === 'all') {
-
-		}
-		else if (filter === 'latest') {
-
-		}
-		else if (filter === 'popular') {
-
-		}
-		else if (filter === 'presell') {
-
-		}
-		else {
-
-		}
-	}, [ArticleParams])
 
 	useEffect(() => {
 		createPagination()
@@ -88,7 +74,7 @@ export default function Articles({ filter, showPagination }: ArticleProp) {
 			{filter === 'latest' || filter === 'popular' || filter === 'presell'
 				? <Swiper spaceBetween={5} slidesPerView={1} modules={[SwiperPagination]} pagination={{ clickable: true }}
 					breakpoints={{ 1280: { slidesPerView: 3 }, 768: { slidesPerView: 2 }, 550: { slidesPerView: 1 } }}>
-					{currentArticles?.map(article =>
+					{latestArticles?.map((article: ArticleType) =>
 						<SwiperSlide key={article.id}>
 							<ArticleCard id={article.id} image={article.image} title={article.title} context={article.context} />
 						</SwiperSlide>
@@ -96,9 +82,9 @@ export default function Articles({ filter, showPagination }: ArticleProp) {
 				</Swiper>
 				: <Box className="my-auto py-8" sx={{ backgroundColor: theme.palette.thirdColor.light }}>
 					<BorderOne title="مقالات">
-						{currentArticles?.length === 0 && <Alert variant="filled" severity="info">مقاله ای جهت نمایش وجود ندارد</Alert>}
+						{articles?.length === 0 && <Alert variant="filled" severity="info">مقاله ای جهت نمایش وجود ندارد</Alert>}
 						<Grid container justifyContent="center" spacing={{ xs: 2, md: 3 }} >
-							{currentArticles?.map(article =>
+							{articles?.map(article =>
 								<Grid key={article.id} item xs={10} sm={6} md={4} lg={4}>
 									<ArticleCard id={article.id} image={article.image} title={article.title} context={article.context} />
 								</Grid>
--- src/Hooks/ArticleHook.tsx
+++ src/Hooks/ArticleHook.tsx
@@ -12,6 +12,29 @@ const useArticle = (id?: string) => {
     })
 }
 
+const useArticleFilter = (filter: string) => {
+  switch (filter) {
+    case 'latest':
+      return useQuery('Article/latest', async () => {
+        return apiRequests.get(`ArticleData?_limit=4`).then(res => res.data);
+      })
+
+    default:
+      return useQuery('Article/latest', async () => {
+        return apiRequests.get(`ArticleData?_limit=4`).then(res => res.data);
+      })
+  }
+}
+
+const useArticlePagination = (currentPage?: number, perPage?: number) => {
+  return useQuery(['Article', currentPage], async () => {
+    return apiRequests.get(`ArticleData?_page=${currentPage}&_per_page=${perPage}`).then(res => res.data);
+  },
+    {
+      keepPreviousData: true,
+    }
+  )
+}
 
 
-export { useArticle }
+export { useArticle, useArticlePagination, useArticleFilter }
